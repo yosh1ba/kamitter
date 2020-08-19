2a47b8b05b45dcdc9cf26218c553be37bfbfb17a
yosh1ba <hk.r8.718@gmail.com>
2020-08-20 07:08:33 +0900

    [Add] heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+      Schema::defaultStringLength(191);   //追加
+      if (\App::environment('production')) {
+        \URL::forceScheme('https');
+      }
     }
 }
